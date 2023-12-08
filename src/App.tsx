1e28a1453eb6d93c912acb562e07b2fb6e6bf461
Zack Ames <zames6584@gmail.com>
2023-12-08 10:10:51 -0500

    its works?

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Box, Torus } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
+import { Suspense } from "react";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      hello
-    </div>
+    <Canvas>
+      <Suspense>
+        <Physics debug>
+          <RigidBody colliders={"hull"} restitution={2}>
+            <Torus />
+          </RigidBody>
+
+          <CuboidCollider position={[0, -2, 0]} args={[20, 0.5, 20]} />
+        </Physics>
+      </Suspense>
+    </Canvas>
   );
-}
+};
 
 export default App;
